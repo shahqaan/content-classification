b46c28d62f4dbcdc445ba97a89df291d8625f026
Shahqaan Qasim <shahqaanqasim@gmail.com>
2016-12-27 18:08:14 +0500

    Display data

--- client/index.jsx
+++ client/index.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
 import {render} from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+  }
+
+  ArticlesList() {
+    return axios.get('/api/v1/articles').then(data => this.setState({articles: data.data}));
+  }
+
+  componentDidMount() {
+    this.ArticlesList();
+  }
+
+  makeKeywords(keywords) {
+
+    let renderedKeywords = keywords.map((keyword, i) => {
+      return keyword.keyword;
+    });
+
+    return renderedKeywords.join(', ');
+  }
+
   render () {
-    return <p> Content Classification</p>;
+
+    if (this.state.articles) {
+      const articles = this.state.articles.map((article, i) => {
+        return (<tr key={i} >
+          <td>{article.url}</td>
+          <td>{this.makeKeywords(article.keywords)}</td>
+        </tr>);
+      });
+      return <table>{articles}</table>;
+    } else {
+      return <table></table>
+    }
+
+
   }
 }
 
